e58106bf16df31cfa55ff68d759066f053655c8d
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-01-23 13:55:41 +0100

    Code done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        String dlouheCislo = "5316017653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145356828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963891";
+
+        long nejvetsiSoucin = najdiNejvetsiSoucin(dlouheCislo, 13);
+        System.out.println(nejvetsiSoucin);
+    }
+
+    private static long najdiNejvetsiSoucin(String cislo, int delkaSouboru) {
+        long nejvetsiSoucin = 0;
+
+        for (int i = 0; i <= cislo.length() - delkaSouboru; i++) {
+            String podretezec = cislo.substring(i, i + delkaSouboru);
+            long soucin = vypocitejSoucin(podretezec);
+
+            if (soucin > nejvetsiSoucin) {
+                nejvetsiSoucin = soucin;
+            }
         }
+
+        return nejvetsiSoucin;
+    }
+
+    private static long vypocitejSoucin(String cislo) {
+        long soucin = 1;
+
+        for (int i = 0; i < cislo.length(); i++) {
+            int cislice = Character.getNumericValue(cislo.charAt(i));
+            soucin *= cislice;
+        }
+
+        return soucin;
     }
 }
